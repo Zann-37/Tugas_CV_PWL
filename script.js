70da8597615e4cc4b24c499d25aac62ae7bfd69d
Muhammad Izyan <mhmdizyan7@gmail.com>
2026-02-08 21:57:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+    window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
+    });
+});
